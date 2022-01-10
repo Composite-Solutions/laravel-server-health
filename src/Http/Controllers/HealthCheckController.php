b6b15a3248e188667de5c13a699279a188ed68ea
Karácsony Márton <41687291+KaracsonyMarton@users.noreply.github.com>
2022-01-10 13:43:23 +0100

    [update] try catch added to HealthCheckController

--- src/Http/Controllers/HealthCheckController.php
+++ src/Http/Controllers/HealthCheckController.php
@@ -3,17 +3,23 @@
 namespace Composite\ServerHealth\Http\Controllers;
 
 use Composite\ServerHealth\ServerHealth;
+use Exception;
 use PragmaRX\Health\Http\Controllers\Health;
 
 class HealthCheckController extends Health
 {
     public function index()
     {
-        $serverStatus = (new ServerHealth())->checkHealth();
-        $appStatus = $this->check();
-        return [
-            "serverStatus" => $serverStatus,
-            "appStatus" => $appStatus
-        ];
+        try {
+            $serverStatus = (new ServerHealth())->checkHealth();
+            $appStatus = $this->check();
+            return [
+                "serverStatus" => $serverStatus,
+                "appStatus" => $appStatus
+            ];
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+
     }
 }
