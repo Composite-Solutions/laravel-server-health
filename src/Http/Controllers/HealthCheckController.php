9f0b18328c0ca3a6fb4dbbc36751258717b50032
Karácsony Márton <41687291+KaracsonyMarton@users.noreply.github.com>
2022-02-04 15:52:38 +0100

    [update] log reader now accepts exact date

--- src/Http/Controllers/HealthCheckController.php
+++ src/Http/Controllers/HealthCheckController.php
@@ -5,16 +5,23 @@ namespace Composite\ServerHealth\Http\Controllers;
 use Composite\ServerHealth\LogReader;
 use Composite\ServerHealth\ServerHealth;
 use Exception;
+use Illuminate\Support\Facades\Request;
 use PragmaRX\Health\Http\Controllers\Health;
 
 class HealthCheckController extends Health
 {
-    public function index()
+    public function index(Request $request)
     {
+        $content = Request::all();
         try {
             $serverStatus = (new ServerHealth())->checkHealth();
             $appStatus = $this->check();
-            $logs = (new LogReader())->get();
+
+            if (isset($content['date'])) {
+                $logs =  (new LogReader(['date' => $content['date']]))->get();
+            } else {
+                $logs =  (new LogReader())->get();
+            }
             return [
                 "serverStatus" => $serverStatus,
                 "appStatus" => $appStatus,
